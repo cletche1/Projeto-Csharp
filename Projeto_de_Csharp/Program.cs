39f837908bda2e61e6426559fd664aeae81bbaf8
cletche <cletche@diaspora.black>
2020-06-17 02:27:04 -0300

    segundo committtt

--- Projeto_de_Csharp/Program.cs
+++ Projeto_de_Csharp/Program.cs
@@ -6,8 +6,19 @@ namespace Projeto_de_Csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Olá Mundo!");
-            Console.ReadKey();
+            //Console.WriteLine("Olá Mundo!");
+            //Console.ReadKey();
+
+            string nome = "Adalberto";
+            int idade = 40;
+            char genero = 'M';
+            Console.WriteLine(nome +" tem "+ idade +" anos "+ " do sexo "+ genero);
+
+            Console.WriteLine("Favor precione qualquer tecla para sair: ");
+
+            Console.ReadLine();
+
+
         }
     }
 }
