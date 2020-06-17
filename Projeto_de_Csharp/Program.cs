fffbd6ff44870c0bcab4bb64ec80a0b6aa3be2fe
cletche <cletche@diaspora.black>
2020-06-17 02:36:28 -0300

    manga

--- Projeto_de_Csharp/Program.cs
+++ Projeto_de_Csharp/Program.cs
@@ -11,13 +11,17 @@ namespace Projeto_de_Csharp
 
 
             //aqui começa codigo
-            string nome = "Adalberto";
-            int idade = 40;
-            char genero = 'M';
-            Console.WriteLine(nome +" tem "+ idade +" anos "+ " do sexo "+ genero);
+            //string nome = "Adalberto";
+            //int idade = 40;
+            //char genero = 'M';
+            //Console.WriteLine(nome +" tem "+ idade +" anos "+ " do sexo "+ genero);
 
-            Console.WriteLine("Favor precione qualquer tecla para sair: ");
+            //Console.WriteLine("Favor precione qualquer tecla para sair: ");
 
+            //Console.ReadLine();
+
+            string[] frutas = new string[3] { "Laranja", "Pera", "Uva" };
+            Console.WriteLine("A fruta numero dois:" + frutas[1]);
             Console.ReadLine();
 
 
